79e386c3971c2c0bc26e0deec4857d2148ddd4ac
Douglas <s08422214@gmail.com>
2021-05-10 19:47:11 -0400

    Created Display interface.

---
+++ src/chip8/emulator/Display.java
@@ -0,0 +1,10 @@
+package chip8.emulator;
+
+/**Public interface for GUI emulator display implementation.
+ * @author Douglas T. | GitHub: Pokepeople02
+ */
+public interface Display {
+	
+	public void update();
+	
+}//end interface Display
